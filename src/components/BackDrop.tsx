3077529da93c8535ee409b2df752a933a85dabff
Rodolfo Sandoval <rodolfo.rudy.sandoval@gmail.com>
2023-06-01 03:49:47 -0700

    init project with background

---
+++ src/components/BackDrop.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import Spline from '@splinetool/react-spline';
+
+export default function BackDrop() {
+  return (
+    <Spline scene="https://prod.spline.design/mfczDwdvP-YfEmcr/scene.splinecode" />
+  );
+}
