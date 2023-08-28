a520a182ca343470e0082274b5670eccdff1822c
Rodolfo Sandoval <rodolfo.rudy.sandoval@gmail.com>
2023-08-28 16:07:56 -0700

    bug fixes

--- src/components/Mobile.tsx
+++ src/components/Mobile.tsx
@@ -6,7 +6,7 @@ import { IoWarning } from "react-icons/io5"
 
 export default function Mobile() {
   return (
-    <div className="flex justify-center items-center h-full w-screen">
+    <div className="flex justify-center items-center h-screen w-screen">
       <div className=" flex flex-col items-center w-[90%] h-[95%] bg-black/40 rounded backdrop-blur-lg overflow-hidden">
         <div className="w-4/5 mt-6">
           <Image src="/images/etc/logo_wide.png" alt="Rudy" width={1342} height={578} />
